612b308df86e23da746a508a9fd6ad6f69b0e879
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-09-10 09:50:07 +0700

    generate token and generate auth token

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,17 +1,22 @@
 export default {
-    port: process.env.PORT,
-    mongodb: process.env.MONGO_URL,
-    cloudinary: { 
-      cloud_name: process.env.CLOUD_NAME, 
-      api_key: process.env.CLOUD_KEY,
-      api_secret: process.env.CLOUD_SECRET
-    },
-    redis: {
-      port: parseInt(process.env.REDIS_PORT as string),
-      host: process.env.REDIS_HOST,
-      username: process.env.REDIS_USERNAME,
-      password: process.env.REDIS_PASSWORD,
-      db: 0
+  port: process.env.PORT,
+  mongodb: process.env.MONGO_URL,
+  cloudinary: { 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_SECRET
+  },
+  redis: {
+    port: parseInt(process.env.REDIS_PORT as string),
+    host: process.env.REDIS_HOST,
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    db: 0
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    accessExpires: process.env.JWT_ACCESS_EXPIRES,
+    refreshExpires: process.env.JWT_REFRESH_EXPIRES
   }
     
 }
---
+++ src/services/token.service.ts
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken"
+import config from "../config/config"
+import * as CacheService from "../services/cache.service"
+import ms from "ms"
+
+export const generateToken = (
+    id: string,
+    role: 'admin' | 'user',
+    expiresIn: string | number,
+    secretKey: string = config.jwt.secret as string
+) => {
+    const payload = {
+        id,
+        role
+    }
+    return jwt.sign(payload,secretKey,{expiresIn})
+}
+
+
+
+export const generateAuthAdminToken = async (accountId: string) => {
+    const role = 'admin'
+    const accessToken = generateToken(accountId,role,config.jwt.accessExpires as string) 
+
+    const refreshToken = generateToken(accountId,role,config.jwt.refreshExpires as string) 
+
+    const ttlInSecond = ms(config.jwt.refreshExpires as string) / 1000
+    await CacheService.setCache(`${role}:${accountId}`,ttlInSecond,refreshToken)
+
+    
+    return {
+        token: {
+            accessToken,
+            refreshToken
+        }
+    }
+}
